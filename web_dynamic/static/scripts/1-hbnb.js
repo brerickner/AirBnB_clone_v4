45ba47059021a74d885784fa67c2a981db7a0473
aydentownsley <aydentownsley@gmail.com>
2021-05-16 16:22:58 +0000

    adding div wrappert to make overflow work, semistandarded files

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,30 +1,17 @@
-$(document).ready( function () {
-  let list = []
-  let list_id = []
-  $("input:checkbox").click(function () {
-    if($(this).is(":checked")){
-      if($(this).val() === "on") {
-        list.push(" " + $(this).attr('data-name'));
-        list_id.push($(this).attr('data-id'))
-        $("DIV.amenities h4").text(list);
-      }} else {
-        list.pop($(this).attr('data-name'));
-        list_id.pop($(this).attr('data-id'))
-        $("DIV.amenities h4").text(list);
-    };
+$(document).ready(function () {
+  const list = [];
+  const listId = [];
+  $('input:checkbox').click(function () {
+    if ($(this).is(':checked')) {
+      if ($(this).val() === 'on') {
+        list.push(' ' + $(this).attr('data-name'));
+        listId.push($(this).attr('data-id'));
+        $('DIV.amenities h4').text(list);
+      }
+    } else {
+      list.pop($(this).attr('data-name'));
+      listId.pop($(this).attr('data-id'));
+      $('DIV.amenities h4').text(list);
+    }
   });
 });
-
-
-/*
-$( document ).ready( function () {
-  $( "DIV.amenities li input" ).click(function( event ) {
-  var target = $( event.target );
-  if ( target.is( ":checked" ) ) {
-    alert(target.attr("data-name"));
-  } else {
-    $( 'DIV.amenities h4' ).text("removed");
-  }
-  });
-});
-*/
